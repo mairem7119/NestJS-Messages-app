b1212198746561adc55c1607681cd7dcbd76dc94
LonNV <nguyenvanlon444@gmail.com>
2024-05-22 18:09:28 +0900

    Add Validation Pipes 1

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param} from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+
+    @Get()
+    listMessages(){
+
+    }
+
+    @Post()
+    createMessages(@Body() body: any){
+        console.log(body);
+    }
+
+    @Get('/:id')
+    getMessages(@Param('id') id: string){
+        console.log(id);
+    }
+}
